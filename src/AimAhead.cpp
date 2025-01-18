582b707aafb20b8d29dfb1504b3f59ea9fc59a0b
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2025-01-18 14:38:29 +0200

    Fix typo

--- src/AimAhead.cpp
+++ src/AimAhead.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[]) {
 
 
     if (!capture::InitializeCapture()) {
-        throw std::runtime_error("Failed to start screen captuer.");
+        throw std::runtime_error("Failed to start screen capture.");
         return 1;
     }
 
