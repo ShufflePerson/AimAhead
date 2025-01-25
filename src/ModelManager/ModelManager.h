c9653da76881dd6c7f37924760cbf4b7674b770d
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2025-01-25 21:42:17 +0200

    Added `model_manager` namespace
    Forgot to commit it before

---
+++ src/ModelManager/ModelManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <filesystem>
+
+#include "../Utils/Utils.h"
+
+namespace model_manager {
+	void update_models_list();
+	std::vector<std::string> get_loaded_models();
+	std::vector<std::string> get_all_models();
+}
