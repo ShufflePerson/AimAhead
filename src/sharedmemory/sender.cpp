6ff539c460c24733c1ccc26437d846dfb63d8007
Shuffle <121307743+ShufflePerson@users.noreply.github.com>
2025-02-21 15:27:13 +0200

    Changed  SharedMemory page name, expanded

--- src/sharedmemory/sender.cpp
+++ src/sharedmemory/sender.cpp
@@ -7,15 +7,20 @@ namespace sharedmemory {
     SenderContext* senderContext;
     uint32_t last_tick = 0;
     void init_sender() {
-        const wchar_t* sharedMemoryName = L"MySharedMemory";
+        const wchar_t* sharedMemoryName = L"XX9922SS";
         senderContext = initSender(sharedMemoryName);
         if (senderContext == nullptr) {
             return;
         }
     }
 
-    void send_data(int32_t x, int32_t y, uint16_t action) {
-        sendData(senderContext, x, y, last_tick, action);
-        last_tick++;
+    void send_data(int32_t x, int32_t y, bool send_mouse, bool down) {
+        sendData(senderContext, x, y, last_tick, send_mouse, down);
+        last_tick = last_tick + 2;
+    }
+
+
+    ITextureData* get_texture_data() {
+        return &senderContext->pSharedData->m_cTextureData;
     }
 }
